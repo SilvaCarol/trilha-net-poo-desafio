84d806c4288cec1e668336eacbcc38e80313ec8b
Caroline Silva <caroline.cristina.silva@hotail.com>
2022-09-29 14:11:42 -0300

    trilha-net-poo-desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "145", modelo: "Modelo 12", iMEI: "3242545", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone");
+Smartphone iphone = new Iphone(numero: "690", modelo: "Modelo 33", iMEI: "23423423", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
